674a8d45f5299b4ea58c46310159db03f456082c
orlandorene619 <orlandorene619@gmail.com>
2022-05-23 18:04:27 -0400

    actual

--- bin/server.js
+++ bin/server.js
@@ -3,6 +3,6 @@ const app = require('../app');
 const server = http.createServer(app);
 
 
-const port = parseInt(process.env.port) || 3800;
+const port = parseInt(process.env.port) || 5432;
 app.set('port', port);
 server.listen(port);
