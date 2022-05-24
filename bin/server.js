c8ef3dc40cbfc7944ed3d5176a42a4e8761f1bb6
orlandorene619 <orlandorene619@gmail.com>
2022-05-24 07:12:20 -0400

    port cambiado

--- bin/server.js
+++ bin/server.js
@@ -2,7 +2,12 @@ const http = require('http');
 const app = require('../app');
 const server = http.createServer(app);
 
-
+/*
 const PORT = parseInt(process.env.PORT) || 3000;
 app.set('PORT', PORT);
-server.listen(PORT);
+server.listen(PORT);*/
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
